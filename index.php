9cae7b62853382ff95f4198426152546a7d029a1
layo13 <liodu13170@gmail.com>
2013-11-30 02:45:50 +0100

    MAJ 2 Layout principal

--- index.php
+++ index.php
@@ -4,15 +4,16 @@
         <meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>FEGProject</title>
-		<link rel="stylesheet" href="./css/cyborg.css" />
-		<script type="text/javascript" src="./js/jquery-1.10.2.min.js"></script>
-		<script type="text/javascript" src="./js/jquery-ui-1.10.3.custom.min.js"></script>
+		<link rel="stylesheet" href="./css/bootstrap.min.css" />
+		<link rel="stylesheet" href="./css/bootstrap-theme.min.css" />
+		<script type="text/javascript" src="./js/jquery-1.10.2.js"></script>
 		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
     </head>
     <body>
 		<div class="container">
 			<div class="jumbotron">
 				<h1>PHP FTW</h1>
+				<p class="text-muted">Bootstrap For Life</p>
 			</div>
 		</div>
     </body>
