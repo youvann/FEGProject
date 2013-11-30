0f0eeb034c9e491b0ff5ab0c20ce21e0d5ffcda8
layo13 <liodu13170@gmail.com>
2013-11-30 02:41:21 +0100

    MAJ Layout principal

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FEGProject</title>
+		<link rel="stylesheet" href="./css/cyborg.css" />
+		<script type="text/javascript" src="./js/jquery-1.10.2.min.js"></script>
+		<script type="text/javascript" src="./js/jquery-ui-1.10.3.custom.min.js"></script>
+		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    </head>
+    <body>
+		<div class="container">
+			<div class="jumbotron">
+				<h1>PHP FTW</h1>
+			</div>
+		</div>
+    </body>
+</html>
