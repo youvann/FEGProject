163affdcad8f447dd67a9f8b51436736da943fac
layo13 <liodu13170@gmail.com>
2013-12-01 21:24:50 +0100

    Reduction de code

--- js/feg.js
+++ js/feg.js
@@ -1,30 +1,22 @@
-$(document).ready(OnReady);
+$(document).ready(function() {
+	$("form").submit(function() {
+		$.ajax({
+			type: $(this).attr("method"),
+			url: $(this).attr("action"),
+			data: $(this).serialize(),
+			success: function() {
+				var result = $("#result_form1");
+				result.hide();
+				if (data.reponse === "ok") {
+					result.html('<br/> Formations bien sélectionnées : ' + data.liste1 + ' et ' + data.liste2);
 
-function OnReady(){
-    // Formulaire 1 : Listes déroulantes
-    $("form").submit(OnSubmit);
-}   
-
-function OnSubmit(data){
-    $.ajax({
-        type: $(this).attr("method"),
-        url: $(this).attr("action"),
-        data: $(this).serialize(),
-        success: OnSuccessForm1
-    });
-    return false;
-} // OnSubmit ()
-
-function OnSuccessForm1(data){
-    var result = $("#result_form1");
-    result.hide();      
-    if(data.reponse === "ok"){
-        result.html('<br/> Formations bien sélectionnées : ' + data.liste1 + ' et ' + data.liste2);
-
-    }else{
-        result.html('<br/> Erreur : Veuillez resaisir les données');
-    }
-    result.fadeIn('slow');
-    
-}   // OnSuccessForm ()
+				} else {
+					result.html('<br/> Erreur : Veuillez resaisir les données');
+				}
+				result.fadeIn('slow');
+			}
+		});
+		return false;
+	});
+});
 
