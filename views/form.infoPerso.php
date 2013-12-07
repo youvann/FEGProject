0aa71b68ff20a455ccab3243b48b9fe0f3c28bb7
youvann <measkevin@gmail.com>
2013-12-07 22:02:14 +0100

    validation formualire infoPerso

--- views/form.infoPerso.php
+++ views/form.infoPerso.php
@@ -26,11 +26,11 @@ ob_start();
         <div class="row">
             <div class="col-md-6">
                 <label for="lastName">Nom</label>
-                <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Votre nom" data-validation="required" />
+                <input type="text" class="form-control required" id="lastName" name="lastName" placeholder="Votre nom" data-validation="required" />
             </div>
             <div class="col-md-6">
                 <label for="firstName">Prénom</label>
-                <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Votre prenom" data-validation="required"/>
+                <input type="text" class="form-control required" id="firstName" name="firstName" placeholder="Votre prenom" data-validation="required" />
             </div>
         </div>
     </div>
@@ -39,15 +39,15 @@ ob_start();
         <div class="row">
             <div class="col-md-4">
                 <label for="birthDate">Date de naissance</label>
-                <input type="date" class="form-control" id="birthDate" name="birthDate" />
+                <input type="text" class="form-control required" id="birthDate" name="birthDate" placeholder="jj/mm/aaaa" data-validation="custom" data-validation-regexp="(^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$)" data-validation-error-msg="Veuillez entrer une date valide"/>
             </div>
             <div class="col-md-4">
                 <label for="birthPlace">Lieu de naissance</label>
-                <input type="text" class="form-control" id="birthPlace" name="birthPlace" placeholder="Votre lieu de naissance" />
+                <input type="text" class="form-control required" id="birthPlace" name="birthPlace" placeholder="Votre lieu de naissance" data-validation="required"/>
             </div>
             <div class="col-md-4">
                 <label for="nationality">Nationalité</label>
-                <input type="text" class="form-control" id="nationality" name="nationality" placeholder="Votre nationalité" />
+                <input type="text" class="form-control required" id="nationality" name="nationality" placeholder="Votre nationalité" data-validation="required"/>
             </div>
         </div>
     </div>
@@ -56,7 +56,7 @@ ob_start();
         <div class="row">
             <div class="col-md-8">
                 <label for="ineNum">N° INE (pour étudiants en France)</label>
-                <input type="text" class="form-control" id="ineNum" name="ineNum" placeholder="Votre numéro INE" />
+                <input type="text" class="form-control required" id="ineNum" name="ineNum" placeholder="Ex : 1234567891A" data-validation="custom" data-validation-regexp="(^[0-9]{10}[a-zA-Z]$)" data-validation-error-msg="Veuillez entrer un numéro d'INE valide"/>
             </div>
         </div>
     </div>  
@@ -70,7 +70,7 @@ ob_start();
         <div class="row">
             <div class="col-md-4">
                 <label for="adress ">Adresse</label>
-                <input type="text" class="form-control" id="adress" name="adress" placeholder="Votre adresse" />
+                <input type="text" class="form-control required" id="adress" name="adress" placeholder="Votre adresse" data-validation="required"/>
             </div>
             <div class="col-md-4">
                 <label for="adressBis">Complément d'adresse</label>
@@ -78,7 +78,7 @@ ob_start();
             </div>
             <div class="col-md-4">
                 <label for="postalCode">Code postal</label>
-                <input type="text" class="form-control" id="postalCode" name="postalCode" placeholder="Votre code postal" />
+                <input type="text" class="form-control required" id="postalCode" name="postalCode" placeholder="Votre code postal" data-validation="custom" data-validation-regexp="(^[0-9]{5}$)" data-validation-error-msg="Veuillez entrer un code postal valide"/>
             </div>
         </div>
     </div>
@@ -87,7 +87,7 @@ ob_start();
         <div class="row">
             <div class="col-md-8">
                 <label for="city">Ville</label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Votre ville" />
+                <input type="text" class="form-control required" id="city" name="city" placeholder="Votre ville" data-validation="required"/>
             </div>
         </div>    
     </div>
@@ -99,11 +99,11 @@ ob_start();
             </div>
             <div class="col-md-4">
                 <label for="mobilePhone">Téléphone mobile</label>
-                <input type="tel" class="form-control" id="mobilePhone" name="mobilePhone"  placeholder="Votre numéro"/>
+                <input type="tel" class="form-control required" id="mobilePhone" name="mobilePhone"  placeholder="Votre numéro" data-validation="custom" data-validation-regexp="(^(06|07)[0-9]{8}$)" data-validation-error-msg="Veuillez entrer un numéro valide"/>
             </div>
             <div class="col-md-4">
                 <label for="postalCode">Adresse électronique</label>
-                <input type="email" class="form-control" id="mail" name="mail" placeholder="Votre mail" data-validation="email"/>
+                <input type="email" class="form-control required" id="mail" name="mail" placeholder="Votre mail" data-validation="email"/>
             </div>
         </div>
     </div>
@@ -115,7 +115,7 @@ ob_start();
         <div class="row">
             <div class="col-md-8">
                 <label for="currentActivity">Activité actuelle</label>
-                <input type="text" class="form-control" id="currentActivity" name="currentActivity" placeholder="étudiant, salarié, demandeur d'emploi, autre" />
+                <input type="text" class="form-control required" id="currentActivity" name="currentActivity" placeholder="étudiant, salarié, demandeur d'emploi, autre" data-validation="required" />
             </div>
         </div>
     </div>
