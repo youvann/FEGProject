fc9a289ed841644fd36b3ece4a62d7ad3e5a0c86
layo13 <liodu13170@gmail.com>
2014-01-13 20:59:00 +0100

    Un petit commit pour le plaisir ^^

--- classes/Translator/TranslatorResultsetToStructure.class.php
+++ classes/Translator/TranslatorResultsetToStructure.class.php
@@ -1,37 +1,43 @@
 <?php
 
-
+/**
+ * @Project: FEG Project
+ * @File: /classes/Translator/TranslatorResulsetToStructure.class.php
+ * @Purpose: Cette classe a pour rôle de traduire le résultat de la 
+ * requête demandant les informations supplémentaires en tableau PHP.
+ * @Author: Lionel Guissani
+ */
 class TranslatorResulsetToStructure {
+
 	/**
-	 * 
+	 * Traduire le résultat de la requête demandant 
+	 * les informations supplémentaires en tableau PHP.
 	 * @param PDOStatement $resultset
-	 * @return array
+	 * @return array Structure des informations supplémentaires
 	 */
 	public function translate(PDOStatement $resultset) {
-		return array('un' => 'toto');
+		$structure = array();
+
+		$i = 0;
+		while ($i < count($resultset)) {
+			$array = array();
+			$array[] = $resultset[$i]['idInfo'];
+			$array[] = $resultset[$i]['libelleInfo'];
+			$array[] = $resultset[$i]['typeInfo'];
+			if ($resultset[$i]['typeInfo'] == 'CheckBoxGroup' || $rs[$i]['typeInfo'] == 'RadioButtonGroup') {
+				$idInfo = $rs[$i]['idInfo'];
+				$libellesInfo = array();
+				while ($i < count($resultset) && $resultset[$i]['idInfo'] === $idInfo) {
+					$libellesInfo[] = $resultset[$i]['libellesInfo'];
+					++$i;
+				}
+				$array[] = $libellesInfo;
+				$i = $i - 1;
+			}
+			++$i;
+			$structure[] = $array;
+		}
+		return $structure;
 	}
 
-	//==================
-	// Méthodes privées
-	//==================
-
-	private function textBoxResulsetToStructure() {
-		return;
-	}
-
-	private function textAreaResulsetToStructure() {
-		return;
-	}
-	
-	private function checkBoxGroupResulsetToStructure() {
-		return;
-	}
-
-	private function checkBoxResulsetToStructure() {
-		return ;
-	}
-	
-	private function radioButtonGroupResulsetToStructure() {
-		return;
-	}
 }
