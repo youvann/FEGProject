598678317659e7221c5511c6002a9a6b3515957a
layo13 <liodu13170@gmail.com>
2014-01-17 13:50:24 +0100

    Ajout du main controller et du formation controller

---
+++ controllers/formation.controller.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @Project: FEG Project
+ * @File: /controllers/main.controller.php
+ * @Purpose:
+ * @Author:
+ */
+if (!isset($_GET['action'])) {
+	$action = "grille";
+} else {
+	$action = $_GET['action'];
+}
+
+/* autorisations
+  $pageAction = array("ordonner", "ajouter", "ajout", "modifier", "modification", "suppression");
+
+  if (in_array($action, $pageAction) && !$utilisateur->isConnected()) {
+  header('location:index.php?uc=utilisateur&action=connecter');
+  } */
+
+switch ($action) {
+	case "grille": {
+			$formations = array();
+			$formations[] = new Formation("Licence Informatique", "L3 Parcours Méthodes Informatiques Appliquées à la Gestion des Entreprises (MIAGE)", "3SIN", "BIN303", "111", "Nicola Olivetti", "Aix-en-Provence", "FEG", "fr");
+			$formations[] = new Formation("Licence Gestion", "L3 Parcours Méthodes Informatiques Appliquées à la Gestion des Entreprises (MIAGE)", "3BGE", "BGE303", "111", "Pierre-Yves Rolland", "Aix-en-Provence", "FEG", "fr");
+			echo $twig->render('formation/grilleFormation.html.twig', array('formations' => $formations));
+		} break;
+	case "ajouter": {
+			echo $twig->render('formation/ajouterFormation.html.twig', array());
+		} break;
+	case "ajout": {
+			$formationManager->insert(new Formation($_POST['mention'], $_POST['etape'], $_POST['code_diplome'], $_POST['code_etape'], $_POST['code_vet'], $_POST['responsable'], $_POST['ville'], $_POST['faculte'], $_POST['langue_pdf']));
+			header('location:index.php?uc=formation&action=grille');
+		} break;
+	case "modifier": {
+			$formation = $formationManager->find($_GET['code_etape'], $_GET['code_vet']);
+			echo $twig->render('formation/ajouterFormation.html.twig', array('formation' => $formation));
+		} break;
+	case "modification": {
+			$formation = new Formation($_POST['mention'], $_POST['etape'], $_POST['code_diplome'], $_POST['code_etape'], $_POST['code_vet'], $_POST['responsable'], $_POST['ville'], $_POST['faculte'], $_POST['langue_pdf']);
+			$formationManager->update($formation);
+			header('location:index.php?uc=formation&action=grille');
+		} break;
+	case "suppression": {
+			$formation = $formationManager->find($_GET['code_etape'], $_GET['code_vet']);
+			$formationManager->delete($formation);
+			header('location:index.php?uc=formation&action=grille');
+		} break;
+	default: break;
+}
--- controllers/main.controller.php
+++ controllers/main.controller.php
@@ -1,62 +1,28 @@
 <?php
+
 /**
  * @Project: FEG Project
  * @File: /controllers/main.controller.php
  * @Purpose:
  * @Author:
  */
-
-if (!isset($_GET['action'])) {
-	$action = "accueil";
+if (!isset($_GET['uc'])) {
+	$uc = "formation";
 } else {
-	$action = $_GET['action'];
+	$uc = $_GET['uc'];
 }
 
-/*/ autorisations
+/* / autorisations
   $pageAction = array("ordonner", "ajouter", "ajout", "modifier", "modification", "suppression");
 
   if (in_array($action, $pageAction) && !$utilisateur->isConnected()) {
   header('location:index.php?uc=utilisateur&action=connecter');
   } */
 
-switch ($action) {
-	case "accueil": {
-			echo $twig->render('accueil.html.twig', array(
-				'titre2' => 'Inscription pour l\'année scolaire 2014-2015'
-			));
+switch ($uc) {
+	case "formation": {
+			require_once './controllers/formation.controller.php';
 		} break;
-    case "form-candidat-formation" : {
-        echo $twig->render('form.formationCandidat.html.twig', array(
-            'titre2' => 'Formulaire d\'inscription pour l\'année soclaire 2014-2015',
-            'formTitle' => 'Etape 1/4 : Parcours de l\'étudiant',
-            'pathAction' => '#',
-            'idForm' => 'form-candidat-formation'
-        ));
-    }break;
-    case "form-info-perso" : {
-        echo $twig->render('form.infoPerso.html.twig', array(
-            'titre2' => 'Formulaire d\'inscription pour l\'année soclaire 2014-2015',
-            'formTitle' => 'Etape 2/4 : Candidature/pré-inscription pour la formation ...',
-            'pathAction' => '#',
-            'idForm' => 'form-info-perso'
-        ));
-    }break;
-    case "form-voeux" : {
-        echo $twig->render('form.voeux.html.twig', array(
-            'titre2' => 'Formulaire d\'inscription pour l\'année soclaire 2014-2015',
-            'formTitle' => 'Etape 3/4 : Choisissez votre spécialité',
-            'pathAction' => '#',
-            'idForm' => 'form-voeux'
-        ));
-    }break;
-    case "form-post-bac" : {
-        echo $twig->render('form.postBac.html.twig', array(
-            'titre2' => 'Formulaire d\'inscription pour l\'année soclaire 2014-2015',
-            'formTitle' => 'Etape 4/4 : Parcours post-bac',
-            'pathAction' => '#',
-            'idForm' => 'form-post-bac'
-        ));
-    }break;
 
 	default: break;
 }
