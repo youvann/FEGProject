75ac4d028310d40ffba2ec2babb1a29eebb33f85
layo13 <liodu13170@gmail.com>
2014-01-20 14:07:47 +0100

    Ajout des nouveaux managers 2

--- Entities/FaireManager.php
+++ Entities/FaireManager.php
@@ -1,6 +1,7 @@
 <?php
 
 class FaireManager {
+
 	private $db;
 
 	function __construct(PDO $db) {
@@ -10,10 +11,25 @@ class FaireManager {
 	public function setDb(PDO $db) {
 		$this->db = $db;
 	}
-	
+
 	public function find();
-	
-	public function insert(Faire $faire);
-	
-	public function delete(Faire $faire);
+
+	public function insert(Faire $faire) {
+		return $this->db->prepare("INSERT INTO `FAIRE` (`CODE_ETAPE`, `INE`, `CODE_FORMATION`, `ORDRE`) VALUES (?, ?, ?, ?);")
+						->execute(array(
+							$faire->CodeEtape(),
+							$faire->Ine(),
+							$faire->CodeFormation(),
+							$faire->Ordre()
+		));
+	}
+
+	public function delete(Faire $faire) {
+		return $this->db->prepare("DELETE FROM `FAIRE` WHERE `CODE_ETAPE` = ? AND `INE` = ?;")
+						->execute(array(
+							$faire->CodeEtape(),
+							$faire->Ine()
+		));
+	}
+
 }
