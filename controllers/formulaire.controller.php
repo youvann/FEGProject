a0869ddf567e20b92fe7df864cdc3a1992286ecc
youvann <measkevin@gmail.com>
2014-01-22 15:39:13 +0100

    update formulaire infoPerso + insert + update document

--- controllers/formulaire.controller.php
+++ controllers/formulaire.controller.php
@@ -33,17 +33,50 @@ switch ($action) {
 
         ));
     } break;
-    case "document": {
-        $documents = $documentManager->findAll();
-        echo $twig->render('formulaire/document.html.twig', array(
-            "documents" => $documents
+    case "documentGeneral": {
+        $documentsGeneraux = $documentGeneralManager->findAll();
+        echo $twig->render('formulaire/documentGeneral.html.twig', array(
+            "documentsGeneraux" => $documentsGeneraux
         ));
+
     } break;
     case "documentSpecifique": {
         $documentsSpecifique = $documentSpecifiqueManager->findAllByFormation("BTM");
         echo $twig->render('formulaire/documentSpecifique.html.twig', array(
             "documentsSpecifique" => $documentsSpecifique
         ));
+    } break;
+    case "traiteInfoPerso": {
+        var_dump($_POST);
+
+        $nom              = $_POST["nom"];
+        $prenom           = $_POST["prenom"];
+        $dateNaissance    = $_POST["date_naissance"];
+        $lieuNaissance    = $_POST["lieu_naissance"];
+        $nationalite      = $_POST["nationalite"];
+        $ine              = $_POST["numero_ine"];
+        $adresse          = $_POST["adresse"];
+        $complement       = $_POST["complement"];
+        $codePostal       = $_POST["code_postal"];
+        $ville            = $_POST["ville"];
+        $fixe             = $_POST["fixe"];
+        $portable         = $_POST["portable"];
+        $mail             = $_POST["mail"];
+        $serieBac         = $_POST["serie_bac"];
+        $anneeBac         = $_POST["annee_bac"];
+        $etablissementBac = $_POST["etablissement_bac"];
+        $departementBac   = $_POST["departement_bac"];
+        $paysBac          = $_POST["pays_bac"];
+        $activite         = $_POST["activite"];
+        $titulaire        = $_POST["titulaire"];
+        $langues          = $_POST["langues"];
+        $autresElements   = $_POST["autres_elements"];
+                               
+        $dossier = new Dossier($ine, "3BAS", $autre, $nom, $prenom, $adresse, $complement, $codePostal, $ville, $dateNaissance, $lieuNaissance, $fixe, $portable, $mail, $langues, $nationalite, $serieBac, $anneeBac, $etablissementBac, $departementBac, $paysBac, $activite, $titulaire, $emploi, NULL, NULL);
+        $dossierManager->insert($dossier);
+    } break;
+    case "traiteChoixSpe": {
+        var_dump($_POST);
     } break;
     default: break;
 }
