2eea595cc19a42dd5ee9caee67503c6a72a4ca7b
layo13 <liodu13170@gmail.com>
2014-02-28 09:38:26 +0100

    update visible doc spe gen

--- Entities/DocumentGeneralManager.php
+++ Entities/DocumentGeneralManager.php
@@ -1,51 +1,62 @@
 <?php
 
 // CHECK
-class DocumentGeneralManager {
+class DocumentGeneralManager
+{
 
 	private $db;
 
-	function __construct(PDO $db) {
+	function __construct(PDO $db)
+	{
 		$this->setDb($db);
 	}
 
-	public function setDb(PDO $db) {
+	public function setDb(PDO $db)
+	{
 		$this->db = $db;
 	}
 
-	public function find($id) {
+	public function find($id)
+	{
 
 		$q = $this->db->prepare("SELECT * FROM `document_general` WHERE `ID` = ?;");
 		$q->execute(array($id));
 		$rs = $q->fetch();
-		return new DocumentGeneral($rs['ID'], $rs['NOM']);
+		return new DocumentGeneral($rs['ID'], $rs['NOM'], $rs['VISIBLE']);
 	}
 
-	public function findAll() {
+	public function findAll()
+	{
 		$documents = array();
 		$rs = $this->db->query("SELECT * FROM `document_general`;")->fetchAll();
 		foreach ($rs as $document) {
-			$documents[] = new DocumentGeneral($document['ID'], $document['NOM']);
+			$documents[] = new DocumentGeneral($document['ID'], $document['NOM'], $document['VISIBLE']);
 		}
 		return $documents;
 	}
 
-	public function insert(DocumentGeneral $document) {
-		return $this->db->prepare("INSERT INTO `document_general` (`NOM`) VALUES (?);")
-						->execute(array($document->getNom()));
+	public function insert(DocumentGeneral $documentGeneral)
+	{
+		return $this->db->prepare("INSERT INTO `document_general` (`NOM`, `VISIBLE`) VALUES (?, ?);")
+			->execute(array(
+				$documentGeneral->getNom(),
+				$documentGeneral->getVisible()));
 	}
 
-	public function update(DocumentGeneral $document) {
-		return $this->db->prepare("UPDATE `document_general` SET `NOM` = ? WHERE `ID` = ?;")
-						->execute(array(
-							$document->getNom(),
-							$document->getId()
-		));
+	public function update(DocumentGeneral $documentGeneral)
+	{
+		return $this->db->prepare("UPDATE `document_general` SET `NOM` = ?, `VISIBLE` = ? WHERE `ID` = ?;")
+			->execute(array(
+				$documentGeneral->getNom(),
+				$documentGeneral->getVisible(),
+				$documentGeneral->getId()
+			));
 	}
 
-	public function delete(DocumentGeneral $document) {
+	public function delete(DocumentGeneral $documentGeneral)
+	{
 		return $this->db->prepare("DELETE FROM `document_general` WHERE `ID` = ?;")
-						->execute(array($document->getId()));
+			->execute(array($documentGeneral->getId()));
 	}
 
 }
--- Entities/DocumentSpecifique.php
+++ Entities/DocumentSpecifique.php
@@ -4,44 +4,96 @@ class DocumentSpecifique {
 	private $id;
 	private $dossierPdf;
 	private $nom;
+	private $visible;
 	private $url;
 	
-	function __construct($id, $dossierPdf, $nom, $url) {
+	function __construct($id, $dossierPdf, $nom, $visible, $url) {
 		$this->id = $id;
 		$this->dossierPdf = $dossierPdf;
 		$this->nom = $nom;
+		$this->visible = $visible;
 		$this->url = $url;
 	}
 
-	public function getId() {
-		return $this->id;
+	/**
+	 * @param mixed $dossierPdf
+	 */
+	public function setDossierPdf($dossierPdf)
+	{
+		$this->dossierPdf = $dossierPdf;
 	}
 
-	public function getDossierPdf() {
+	/**
+	 * @return mixed
+	 */
+	public function getDossierPdf()
+	{
 		return $this->dossierPdf;
 	}
 
-	public function getNom() {
-		return $this->nom;
-	}
-
-	public function getUrl() {
-		return $this->url;
-	}
-
-	public function setId($id) {
+	/**
+	 * @param mixed $id
+	 */
+	public function setId($id)
+	{
 		$this->id = $id;
 	}
 
-	public function setDossierPdf($dossierPdf) {
-		$this->dossierPdf = $dossierPdf;
+	/**
+	 * @return mixed
+	 */
+	public function getId()
+	{
+		return $this->id;
 	}
 
-	public function setNom($nom) {
+	/**
+	 * @param mixed $nom
+	 */
+	public function setNom($nom)
+	{
 		$this->nom = $nom;
 	}
 
-	public function setUrl($url) {
+	/**
+	 * @return mixed
+	 */
+	public function getNom()
+	{
+		return $this->nom;
+	}
+
+	/**
+	 * @param mixed $url
+	 */
+	public function setUrl($url)
+	{
 		$this->url = $url;
 	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	/**
+	 * @param mixed $visible
+	 */
+	public function setVisible($visible)
+	{
+		$this->visible = $visible;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getVisible()
+	{
+		return $this->visible;
+	}
+
+
 }
