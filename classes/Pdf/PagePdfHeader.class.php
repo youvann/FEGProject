1ae2b00d4866a39cb8f5ab74b4fe97120bf0a271
youvann <measkevin@gmail.com>
2014-03-13 14:23:10 +0100

    ajout des commentaires pour les PDF

--- classes/Pdf/PagePdfHeader.class.php
+++ classes/Pdf/PagePdfHeader.class.php
@@ -1,41 +1,55 @@
 <?php
+
 /*
  * @Project: FEG Project
  * @File: /classes/Pdf/PagePdfHeader.class.php
- * @Purpose: Header du PDF
- * @Author:
+ * @Purpose: En-tête du PDF
+ * @Author: Kevin meas
  */
 
 class PagePdfHeader {
-	private $imgPath;
-	private $headerText;
-        
-        /*
-	public function __construct($imgPath, $headerText){
-		$this->imgPath    = $imgPath;
-		$this->headerText = $headerText;
-	}*/
+    /**
+     * @var string Chemin de l'image du PDF (Logo : Faculté d'économie et de Gestion)
+     */
+    private $imgPath;
+    /**
+     * @var string  Texte de l'en-tête du PDF (DOSSIER DE ... ANNEE UNIVERSITAIRE ...)
+     */
+    private $headerText;
 
-	public function setImgPath ($imgPath){
-		$this->imgPath = $imgPath;
-	}
+    /**
+     * Permet de définir le chemin de l'image du PDF
+     *
+     * @param string $imgPath Chemin de l'image
+     */
+    public function setImgPath ($imgPath) {
+        $this->imgPath = $imgPath;
+    }
 
-	public function setHeadertext ($headerText){
-		$this->headerText = $headerText;
-	}
+    /**
+     * Permet de définir le texte de l'en-tête du PDF
+     *
+     * @param string $headerText Texte de l'en-tête du PDF
+     */
+    public function setHeadertext ($headerText) {
+        $this->headerText = $headerText;
+    }
 
-	public function __toString (){
-		return '<page_header> 
+    /**
+     * Affiche l'en-tête du PDF
+     *
+     * @return string En-tête du PDF
+     */
+    public function __toString () {
+        return '<page_header>
 			        <table class="t_header">
 			        <col style="width: 49%">
 			            <tr>
 			                <td><img src="' . $this->imgPath . '" alt="image"></td>
-			                <td class="bold titre4">' . $this->headerText .'</td>
+			                <td class="bold titre4">' . $this->headerText . '</td>
 			            </tr>
 			        </table>
 			    </page_header> ';
-	}
-
+    }
 }
-
 ?>
